7eeb5922f030eeb4fc77dacf05635e4c5429e26b
Camarederic <camarederic@gmail.com>
2026-02-27 20:58:38 +0300

    Added code to the buttons and some more styles JS & CSS

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-'use strict'
+"use strict";
 
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("change");
+});
